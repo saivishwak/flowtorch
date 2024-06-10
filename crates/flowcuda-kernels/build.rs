70fe867047005e97dd354ac547504ad0bbb1dc8c
saivishwak <saivishwak40@gmail.com>
2024-06-10 20:06:59 +0530

    add cuda_bindgen

--- crates/flowcuda-kernels/build.rs
+++ crates/flowcuda-kernels/build.rs
@@ -1,56 +1,7 @@
-use std::env;
-use std::fs;
-use std::path::Path;
-use std::process::Command;
-
 fn main() {
-    // Path to the output directory
-    let out_dir = env::var("OUT_DIR").unwrap();
-
-    // Inform Cargo about the dependency on the generated PTX files
-    println!("cargo:rerun-if-changed=src");
-
-    // Check if nvcc is available
-    let nvcc_available = Command::new("which")
-        .arg("nvcc")
-        .output()
-        .map(|output| output.status.success())
-        .unwrap_or(false);
-
-    if !nvcc_available {
-        eprintln!("Error: nvcc (CUDA compiler) not found. Make sure CUDA Toolkit is installed and added to your PATH.");
-        std::process::exit(1);
-    }
-
-    // Compile CUDA files and generate PTX files in the output directory
-    let src_dir = Path::new("src");
-    let cu_files = fs::read_dir(src_dir)
-        .expect("Failed to read src directory")
-        .filter_map(|entry| {
-            let entry = entry.expect("Failed to read directory entry");
-            let path = entry.path();
-            if path.is_file() && path.extension().unwrap_or_default() == "cu" {
-                Some(path)
-            } else {
-                None
-            }
-        });
-
-    for cu_file in cu_files {
-        let cu_file_name = cu_file.file_name().unwrap().to_str().unwrap();
-        let ptx_file_name = format!("{}.ptx", cu_file.file_stem().unwrap().to_str().unwrap());
-        let ptx_output_path = Path::new(&out_dir).join(&ptx_file_name);
-
-        let output = Command::new("nvcc")
-            .arg("-ptx")
-            .arg("-o")
-            .arg(&ptx_output_path)
-            .arg(&cu_file)
-            .output();
-
-        match output {
-            Ok(_) => println!("{} compiled successfully!", cu_file_name),
-            Err(e) => println!("Failed to compile {}: {}", cu_file_name, e),
-        }
-    }
+    println!("cargo:rerun-if-changed=build.rs");
+    let builder = bindgen_cuda::Builder::default();
+    println!("cargo:info={builder:?}");
+    let bindings = builder.build_ptx().unwrap();
+    bindings.write("src/lib.rs").unwrap();
 }
